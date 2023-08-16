6129b1f0ab037e37d089e6b6a0534e88188b2227
rmanikandan2001 <rmanikandan6867@gmail.com>
2023-08-15 23:00:24 -0700

    add

---
+++ main.js
@@ -0,0 +1,25 @@
+
+	genter= prompt("enter a number");
+ 
+	if(genter=="f"){
+		console.log("she will work only urban area");
+	}
+	
+	else if(genter=="m"){
+		
+		 if(age>20 & age<=40){
+			
+			console.log("may  work in any where");
+			
+		}
+		
+		 if(age>40 & age<=60){
+			
+			console.log("will work in uran area");
+			
+		}
+		
+		else {
+			console.log("error");
+		}
+	}
